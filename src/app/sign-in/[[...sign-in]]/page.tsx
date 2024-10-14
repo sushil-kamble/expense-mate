d19994a27cc91137d6e10057fa732c5954e49757
Sushil <iamsushil303@gmail.com>
2024-10-15 02:28:50 +0530

    Added auth state change

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
     return (
-        <div className="grid place-items-center h-screen">
+        <div className="flex justify-center items-center md:mt-20">
             <SignIn />
         </div>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,31 @@
+'use client';
 import React from 'react';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { UserButton, useAuth } from '@clerk/nextjs';
+import { Skeleton } from '@/components/ui/skeleton';
 
 const Header = () => {
+    const { isLoaded, userId } = useAuth();
+
     return (
         <div className="border-b border-zinc-100 py-3 sticky top-0 z-10 bg-white">
             <div className="container mx-auto flex justify-between items-center">
-                <h2 className="text-2xl font-bold">Expense Tracker</h2>
-                <Button>Sign In</Button>
+                <Link href="/">
+                    <h2 className="text-2xl font-bold">Expense Tracker</h2>
+                </Link>
+                {!isLoaded ? (
+                    <Skeleton className="h-8 w-20" />
+                ) : userId ? (
+                    <UserButton />
+                ) : (
+                    <Link href="/sign-up">
+                        <Button>Sign In</Button>
+                    </Link>
+                )}
             </div>
         </div>
     );
 };
+
 export default Header;
