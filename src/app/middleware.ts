5f1cb51690454117549fb259db6434bb83314fc5
Sushil <iamsushil303@gmail.com>
2024-10-15 01:32:37 +0530

    Added shadcn components

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    // Add more public routes here
+]);
+
+export default clerkMiddleware((auth, request) => {
+    if (!isPublicRoute(request)) {
+        auth().protect();
+    }
+});
 
 export const config = {
     matcher: [
